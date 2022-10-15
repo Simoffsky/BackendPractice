56654593b37c06390a3024ac5a9106b33631e382
Simoffsky <maks-kirsanov26@mail.ru>
2022-10-15 11:47:18 +1000

    added doctor repository interface file + added interface for list in IRepository

--- Domain/Models/Base/IRepository.cs
+++ Domain/Models/Base/IRepository.cs
@@ -1,6 +1,8 @@
 namespace Domain.Models; 
 
 public interface IRepository<T> where T: class {
+
+    public IEnumerable<T> List();
     public T Create(T item);
     public T Get(int id);
     public bool Delete(int id);
---
+++ Domain/Models/Doctor/IDoctorRepository.cs
@@ -0,0 +1,5 @@
+namespace Domain.Models; 
+
+public interface IDoctorRepository {
+    
+}
