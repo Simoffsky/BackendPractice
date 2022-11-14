7df8eecacfb57e8d5f8cfce23818a10ecca52f1a
Simoffsky <maks-kirsanov26@mail.ru>
2022-11-14 16:48:00 +1000

    Entity framework context + initial migration(models)

---
+++ DataBase/ApplicationContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using DataBase.Models;
+namespace DataBase;
+
+public class ApplicationContext : DbContext
+{
+    public DbSet<UserModel> Users { get; set; }
+    public DbSet<DoctorModel> Doctors { get; set; }
+    public DbSet<AppointmentModel> Appointments { get; set; }
+    public DbSet<ScheduleModel> Schedules { get; set; }
+    public DbSet<SpecializationModel> Specializations { get; set; }
+    public ApplicationContext(DbContextOptions options) : base(options) { }
+}
