a5edb30965f56496de9d7b8b10416a8845ec22f5
Simoffsky <maks-kirsanov26@mail.ru>
2022-11-14 18:16:35 +1000

    Added Schedule Repository + Added legacy timestamp behavior in ApplicationContext(Entity framework)

--- DataBase/ApplicationContext.cs
+++ DataBase/ApplicationContext.cs
@@ -9,5 +9,9 @@ public class ApplicationContext : DbContext
     public DbSet<AppointmentModel> Appointments { get; set; }
     public DbSet<ScheduleModel> Schedules { get; set; }
     public DbSet<SpecializationModel> Specializations { get; set; }
-    public ApplicationContext(DbContextOptions options) : base(options) { }
+
+    public ApplicationContext(DbContextOptions options) : base(options) {
+        AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+        AppContext.SetSwitch("Npgsql.DisableDateTimeInfinityConversions", true);
+    }
 }
--- UnitTests/DataBase/DBPlayground.cs
+++ UnitTests/DataBase/DBPlayground.cs
@@ -2,7 +2,7 @@ using DataBase;
 using DataBase.Repositories;
 using Domain.Models;
 
-namespace UnitTests; 
+namespace UnitTests;
 
 public class DBPlayground {
     private readonly ApplicationContextFactory _dbFactory;
@@ -22,7 +22,7 @@ public class DBPlayground {
         repo.Delete(user.Id);
         context.SaveChanges();
     }
-    
+
     [Fact]
     public void UserRepositoryNotExists() {
         var context = _dbFactory.CreateDbContext();
@@ -34,12 +34,13 @@ public class DBPlayground {
         repo.Delete(user.Id);
         context.SaveChanges();
     }
-    
+
     [Fact]
-    public void UserRepositoryPgTest() { // Write here any test
+    public void UserRepositoryPgTest() {
+        // Write here any test
         var context = _dbFactory.CreateDbContext();
         var repo = new UserRepository(context);
-        
+
         var assertList = new List<User>();
         var user = new User("Vasek", "123", 1, "+7914634635", "Vasua Krasnoshek", Role.Patient);
         assertList.Add(user);
@@ -47,20 +48,22 @@ public class DBPlayground {
         user = new User("Pip", "123", 2, "+7914634635", "Pip Krasnoshek", Role.Patient);
         assertList.Add(user);
         repo.Create(user);
-        
+
         context.SaveChanges();
-        
+
         var testList = repo.List().ToList();
         for (int i = 0; i < assertList.Count; ++i) {
             Assert.Equal(testList[i].Id, assertList[i].Id);
             Assert.Equal(testList[i].FullName, assertList[i].FullName);
             repo.Delete(testList[i].Id);
         }
+
         context.SaveChanges();
     }
-    
+
     //[Fact]
-    public void DoctorRepositoryPgTest() { // Write here any test
+    public void DoctorRepositoryPgTest() {
+        // Write here any test
         var context = _dbFactory.CreateDbContext();
         var repo = new DoctorRepository(context);
         var spec = new Specialization(1, "Proktolog");
@@ -68,5 +71,25 @@ public class DBPlayground {
         context.SaveChanges();
         var list = repo.GetBySpec(spec).ToList();
         Assert.Equal(list[0].Id, 1);
+    }
+
+    [Fact]
+    public void ScheduleRepositoryPgTest() {
+        var context = _dbFactory.CreateDbContext();
+        var repo = new ScheduleRepository(context);
+        var schedule = new Schedule(1, 1,
+            new DateTime(2022, 12, 15, 15, 0, 0, 0),
+            new DateTime(2022, 12, 15, 15, 30, 0, 0)); // half hour difference
+        
+        repo.Create(schedule);
+        context.SaveChanges();
+
+        var spec = new Specialization(1, "Proktolog");
+        var test = repo.GetScheduleByDate(new Doctor(1, "Vasua", spec), new DateOnly(2022, 12, 15)).ToList()[0];
+        
+        Assert.True(test.Id == schedule.Id && test.DoctorId == schedule.DoctorId);
+        
+        repo.Delete(schedule.Id);
+        context.SaveChanges();
     }
 }
